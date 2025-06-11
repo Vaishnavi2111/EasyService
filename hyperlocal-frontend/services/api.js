48ec2c9685b85e1a70c77d19eecac6652a188094
Vaishnavi2111 <vaishnaviparab336@gmail.com>
2025-06-11 20:24:09 +0530

    Added mock API support using JSON Server and updated .gitignore

--- hyperlocal-frontend/services/api.js
+++ hyperlocal-frontend/services/api.js
@@ -1,8 +1,9 @@
 // src/services/api.js
 import axios from 'axios';
 
-const API = axios.create({
+const api = axios.create({
   baseURL: import.meta.env.VITE_API_URL, // Now it uses the value from .env
+  withCredentials: true, // include cookies if using auth
 });
 
-export default API;
+export default api;
